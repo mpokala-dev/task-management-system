cae06d30292d120b4ef494edf877fa560c983d48
Madhuri Pokala <madhurid0309@gmail.com>
2026-07-19 22:50:08 +0100

    TMAS-29: implement centralized ThemeProvider exposing design tokens

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { ROUTES } from '@/constants/routes';
+import { useTheme } from '@/contexts/useTheme';
 
 function Home() {
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  const theme = useTheme();
 
   return (
     <>
-      <h1>Home Page {isAuthenticated ? ' logged in' : ' Sign up'}</h1>
+      <h1 style={{ color: theme.colors.primary }}>
+        Home Page {isAuthenticated ? ' logged in' : ' Sign up'}
+      </h1>
       <Link to={ROUTES.LOGIN}>Sign in</Link>
     </>
   );
