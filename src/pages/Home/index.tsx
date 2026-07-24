4fef5036543f34620c4f894b129a5f751ddc8500
Madhuri Pokala <madhurid0309@gmail.com>
2026-07-24 22:31:02 +0100

    TMAS-30 | polish login page | reduce placeholder text weight/size and tighten title-subtitle spacing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { ROUTES } from '@/constants/routes';
 import { useTheme } from '@/contexts/useTheme';
+import Button from '@/components/common/Button/Button';
+import Input from '@/components/common/Input';
+import Card from '@/components/common/Card';
 
 function Home() {
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
@@ -12,6 +15,17 @@ function Home() {
       <h1 style={{ color: theme.colors.primary }}>
         Home Page {isAuthenticated ? ' logged in' : ' Sign up'}
       </h1>
+      <Card className="custom-card">
+        <Input label="Username" placeholder="Enter your username" />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          error="Password is required"
+        />
+        <Input label="Email" type="email" placeholder="Enter your email" />
+        <Button> Click me </Button>
+      </Card>
       <Link to={ROUTES.LOGIN}>Sign in</Link>
     </>
   );
